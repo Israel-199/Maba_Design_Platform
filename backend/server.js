46b1a032686e7dac6c923861b58e2de5272c9eb2
Israel Assefa <israelassefa199@gmail.com>
2025-05-23 16:19:38 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,18 +44,22 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
 }
-app.use(notFound);
 
+app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => {
