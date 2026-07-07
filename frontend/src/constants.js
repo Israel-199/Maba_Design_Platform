6010394cfe9635627be3eae11f1b6801fc8eb8aa
Israel-199 <israelassefa199@gmail.com>
2026-07-07 04:58:53 -0700

    updated tsconfig files

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,8 @@
-export const BASE_URL = "";
-export const PRODUCTS_URL = "/api/products";
-export const USERS_URL = "/api/users";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
-export const UPLOAD_URL = "/api/upload";
-export const RECEIPT_URL = "/api/send";
+export const BASE_URL = "https://maba-design-platform-duws.onrender.com";
+
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
+export const UPLOAD_URL = `${BASE_URL}/api/upload`;
+export const RECEIPT_URL = `${BASE_URL}/api/send`;
